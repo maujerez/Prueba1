9ee90be5ab25f7cdeb3159ff58131d57321b6114
LoreGcamp <141676335+LoreGcamp@users.noreply.github.com>
2023-08-10 17:06:58 -0300

    Saludo

--- src/prueba1/Prueba1.java
+++ src/prueba1/Prueba1.java
@@ -15,7 +15,7 @@ public class Prueba1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hola a todos"); 
     }
     
 }
